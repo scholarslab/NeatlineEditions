2613089f01c76024b12595342825102bf942e7ea
David McClure <david.mcclure@virginia.edu>
2012-03-21 15:24:08 -0400

    tests for edition add and delete

--- tests/integration/ItemsControllerTest.php
+++ tests/integration/ItemsControllerTest.php
@@ -22,8 +22,8 @@ class NLEditions_ItemsControllerTest extends NLEditions_Test_AppTestCase
      */
     public function setUp()
     {
-        parent::setUp();
         $this->setUpPlugin();
+        $this->editionsTable = $this->db->getTable('NeatlineEdition');
     }
 
     /**
@@ -73,7 +73,7 @@ class NLEditions_ItemsControllerTest extends NLEditions_Test_AppTestCase
      *
      * @return void.
      */
-    public function testNeatlineEditionsItemEditTab()
+    public function testItemEditTab()
     {
 
         // Create exhibits.
@@ -113,7 +113,7 @@ class NLEditions_ItemsControllerTest extends NLEditions_Test_AppTestCase
      *
      * @return void.
      */
-    public function testNeatlineEditionsItemEditSelect()
+    public function testItemEditSelect()
     {
 
         // Create exhibits.
@@ -146,6 +146,127 @@ class NLEditions_ItemsControllerTest extends NLEditions_Test_AppTestCase
             '//select[@id="exhibit-id"]/option[@value="'
             . $exhibit2->id . '"][@selected="selected"]',
             'Exhibit 2');
+
+    }
+
+    /**
+     * When an item is added and an exhibit is specified, a new edition
+     * should be created.
+     *
+     * @return void.
+     */
+    public function testEditionCreationOnItemAdd()
+    {
+
+        // Create exhibit.
+        $exhibit = $this->_createExhibit('Exhibit 1');
+
+        // Get starting count.
+        $count = $this->editionsTable->count();
+
+        // Set exhibit id.
+        $this->request->setMethod('POST')
+            ->setPost(array(
+                'public' => 1,
+                'featured' => 0,
+                'Elements' => array(),
+                'order' => array(),
+                'exhibit_id' => $exhibit->id
+            )
+        );
+
+        // Hit item edit.
+        $this->dispatch('items/add');
+
+        // +1 editions.
+        $this->assertEquals($this->editionsTable->count(), $count+1);
+
+    }
+
+    /**
+     * When an item is edited and an exhibit is specified, a new edition
+     * should be created.
+     *
+     * @return void.
+     */
+    public function testEditionCreationOnItemEdit()
+    {
+
+        // Create exhibit.
+        $exhibit = $this->_createExhibit('Exhibit 1');
+
+        // Create item.
+        $item = new Item();
+        $item->save();
+
+        // Get starting count.
+        $count = $this->editionsTable->count();
+
+        // Set exhibit id.
+        $this->request->setMethod('POST')
+            ->setPost(array(
+                'public' => 1,
+                'featured' => 0,
+                'Elements' => array(),
+                'order' => array(),
+                'exhibit_id' => $exhibit->id
+            )
+        );
+
+        // Hit item edit.
+        $this->dispatch('items/edit/' . $item->id);
+
+        // +1 editions.
+        $this->assertEquals($this->editionsTable->count(), $count+1);
+
+        // Get the edition object.
+        $edition = $this->editionsTable->find(1);
+        $this->assertEquals($edition->item_id, $item->id);
+
+    }
+
+    /**
+     * When an item with an edition is edited and the edition is set
+     * to null, the edition should be deleted.
+     *
+     * @return void.
+     */
+    public function testEditionDeleteOnItemEdit()
+    {
+
+        // Create 2x item/exhibit/edition.
+        $item1 = $this->_createItem();
+        $item2 = $this->_createItem();
+        $exhibit1 = $this->_createExhibit('Exhibit 1');
+        $exhibit2 = $this->_createExhibit('Exhibit 2');
+        $edition1 = $this->_createEdition($item1, $exhibit1);
+        $edition2 = $this->_createEdition($item2, $exhibit2);
+
+        // Get starting count.
+        $count = $this->editionsTable->count();
+
+        // Set exhibit id.
+        $this->request->setMethod('POST')
+            ->setPost(array(
+                'public' => 1,
+                'featured' => 0,
+                'Elements' => array(),
+                'order' => array(),
+                'exhibit_id' => 'Select Below'
+            )
+        );
+
+        // Hit item edit.
+        $this->dispatch('items/edit/' . $item1->id);
+
+        // -1 editions.
+        $this->assertEquals($this->editionsTable->count(), $count-1);
+
+        // Re-get the edition objects.
+        $edition1 = $this->editionsTable->find($edition1->id);
+        $edition2 = $this->editionsTable->find($edition2->id);
+        $this->assertNull($edition1);
+        $this->assertNotNull($edition2);
 
     }
 
